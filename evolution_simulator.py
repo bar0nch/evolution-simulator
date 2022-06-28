c0853815c3d6704290c85d1c9cd5c0c897098648
baronch <75022267+bar0nch@users.noreply.github.com>
2022-06-29 00:18:23 +0200

    started updating the simulator in v0.2.2
    Added temperature, states of matter, movement for liquids in the grid, a switchable temperature graphical map (visualized on top of the normal terrain matrix), optional prints in the console activated by a hotkey.

--- evolution_simulator.py
+++ evolution_simulator.py
@@ -1,19 +1,25 @@
-import pygame, time, random, pickle, json, os
+import pygame, time, random, pickle, json, os, copy
 from particle_related_classes import * #classes Particle, Element and Compound
 from world_manager import * #saving and loading functions + Version class and methods
+import world_generator #functions to automatically generate world files
 pygame.init()
 temp = None #general multipurpose variable
-minimum_version_required = 'v0.2.1'
+optional_prints = False
+temperatures_map_alpha = 230
+simulation_slowness_multiplier = 0
+control_sensibility_slowness_multiplier = 0
+minimum_version_required = 'v0.2.2'
 
 #================================================TODO================================================
 '''
-do proper testing with the reaction request system
+create compound unit class
 update reactions in the mainloop
 create reactions and compounds data automatically
 '''
 #===============================================BUGLOG===============================================
 
 '''
+liquids only move in the middle of the world
 '''
 
 #=============================================FUNCTIONS==============================================
@@ -111,7 +117,9 @@ extracted_temp_data = temp.read()
 if extracted_temp_data:
     VERSION.override(extracted_temp_data)
 temp.close()
-missing_by_obsolescence = {'v0.2.1': "ratios json file"}
+missing_by_obsolescence = {'v0.2.1': "ratios json file",
+                           'v0.2.2': "melting point key in particles, temperature matrix, movement (for single units)"
+                           }
 current_missing = [] #checks if the version is obsolete
 for v, feature in missing_by_obsolescence.items():
     ver = WorldVersion()
@@ -137,43 +145,60 @@ world = world_data["world"]
 compounds = world_data["compounds"]
 reactions = world_data["reactions"]
 ratios = world_data["ratios"]
-''' 
-world_data = {"version":VERSION.String,
-              "elements":elements,
-              "compounds":compounds,
-              "reactions":reactions,
-              "ratios":ratios,
-              "w":w,
-              "h":h,
-              "side":side,
-              "spacing":spacing,
-              "max particle ratio":max_particle_ratio,
-              "world":world[:]}
-'''
+temperatures = world_data["temperature"]
+
 win = pygame.display.set_mode((w*side,h*side))
 pygame.display.set_caption("evolution simulator") #generates the grid
 
 
 #=======================================WORLD RELATED CLASSES========================================
 
-class ReacRequest: #the request an unit sends to another to make a reaction together
-    def __init__(self, start_unit, reaction):
-        self.units = {start_unit: False}
+class MoveRequest:
+    def __init__(self, requester, direction=None):
+        self.requester = requester
+        self.subject = None
+        self.direction = direction
         self.accepted = False
-        self.reaction = reaction
 
-    def __str__(self):
-        return "units: "+str({str(unit):state for unit,state in self.units.items()})+"  accepted: "+str(self.accepted)+"  reaction: "+str(self.reaction)
-
-    def close_unit(self,unit):
-        self.units[unit] = True
-        if not bool([i for i in self.units.values() if i == False]): #if there are no False values in self.units
-            total_particles = 0
-            for req in self.reaction["requirements"]:
-                total_particles += req[0]
-            if len(self.units) == total_particles:
-                self.accepted = True
+    def set_direction(self, direction=None):
+        if direction:
+            self.direction = direction
+            return
         
+        if not self.direction: #not to waste resources
+            directions = {"up":False, "down":False, "left":False, "right":False} #dict to check the available directions
+            units_available = {"up":None, "down":None, "left":None, "right":None} #binds the directions to the proper units
+            
+            for neg in self.requester.neighbours: #checks every neighbour of the requester unit and sets their direction to available if it is
+                if neg.state_of_matter == "liquid":
+                    if neg.Y == self.requester.Y - 1 and neg.X == self.requester.X:
+                        directions["up"] = True
+                        units_available["up"] = neg
+                    elif neg.Y == self.requester.Y + 1 and neg.X == self.requester.X:
+                        directions["down"] = True
+                        units_available["down"] = neg
+                    elif neg.X == self.requester.X - 1 and neg.Y == self.requester.Y:
+                        directions["left"] = True
+                        units_available["left"] = neg
+                    elif neg.X == self.requester.X + 1 and neg.Y == self.requester.Y:
+                        directions["right"] = True
+                        units_available["right"] = neg
+                  
+            directions_list = [key for key, value in directions.items() if value] #a list is made to be shuffled and so to randomise the direction chosen
+            if directions_list:
+                random.shuffle(directions_list)
+                self.direction = directions_list[0]
+                self.subject = units_available[self.direction] #select the other unit subject to the movement with the direction bind
+
+    def cancel(self):
+        self.requester.mr = None
+        if self.subject:
+            self.subject.mr = None
+        
+
+class ReacRequest: #wip
+    def __init__(self):
+        pass
 
 class Unit: #the floor units where cells will be standing
     def __init__(self,particle,x,y,terr_matrix,X,Y):
@@ -184,6 +209,8 @@ class Unit: #the floor units where cells will be standing
         self.particle = particle
         self.color = self.particle.color
         self.terrain_matrix = terr_matrix
+        self.state_of_matter = "solid"
+        self.mr = None #MoveRequest
         self.rr = None #ReacRequest
 
     def __str__(self):
@@ -206,54 +233,76 @@ class Unit: #the floor units where cells will be standing
         elif world_data["max particle ratio"] == 4:
             self.neighbours = [self.terrain_matrix[self.X+x][self.Y+y] for x, y in ((-1,0),(0,-1),(0,1),(1,0)) if self.X+x >= 0 and self.X+x < len(self.terrain_matrix[0]) and self.Y+y >= 0 and self.Y+y < len(self.terrain_matrix)]
         else:
-            raise Exception("max particle ratio is set to neither 4 or 8")
+            raise ValueError("max particle ratio is set to neither 4 or 8")
     
         
     def update(self, update_neighbours=False):
         if update_neighbours:
             self.calc_neighbours()
 
-        if (not self.rr) and self.particle in ratios.keys():
-            possible_reactions = []
-            for reac in reactions:
-                if is_in_reac_requirements(self.particle,reac):
-                    possible_reactions.append(reac)
-            if possible_reactions:
-                target_reaction = possible_reactions[random.randint(0,len(possible_reactions)-1)]         
-                self.rr = ReacRequest(self,target_reaction)
+        if temperatures[self.X][self.Y] > self.particle.melting_point: #temperature variability leads to a change in the state of matter of the unit
+            if optional_prints and self.state_of_matter == "solid": #prints the change of state
+                print(self.particle.name, "turned to liquid")
+            self.state_of_matter = "liquid"
+        else:
+            if optional_prints and self.state_of_matter == "liquid": #prints the change of state
+                print(self.particle.name, "turned to solid")
+            self.state_of_matter = "solid"
 
-        elif self.rr:
-            target_reaction = self.rr.reaction
-
-        if self.rr:
-            for neg in self.neighbours:
-                if neg.rr == None and is_in_reac_requirements(neg.particle,target_reaction):
-                    max_particle_amount = None
-                    for req in target_reaction["requirements"]:
-                        if req[1] == neg.particle:
-                            max_particle_amount = req[0]
-                    current_particle_amount = 0
-                    for unit in self.rr.units.keys():
-                        if unit.particle == neg.particle:
-                            current_particle_amount += 1
-                    if max_particle_amount > current_particle_amount:
-                        neg.rr = self.rr
-                        self.rr.units[neg] = False
-                self.rr.close_unit(self)
+        if not self.mr: #moves the unit
+            if self.state_of_matter == "liquid": #only moves liquids
+                if random.randint(0,40) == 0:
+                    self.mr = MoveRequest(self)
+                    self.mr.set_direction()
+                    if self.mr.subject:
+                        self.mr.subject.mr = self.mr
+                        self.mr.accepted = True
 
     def late_update(self):
-        if self.rr:
-            if random.uniform(0,100) > self.rr.reaction["probability"]:
-                self.rr.accepted = False
-            if self.rr.accepted:
-                print("\n\nreaction occourred:")
-                print(self.rr)
-            for unit in self.rr.units.keys():
-                unit.rr = None
+        if self.mr:
+            if self.mr.accepted:
+                self.mr.requester.particle, self.mr.subject.particle = self.mr.subject.particle, self.mr.requester.particle
+                self.mr.requester.color, self.mr.subject.color = self.mr.subject.color, self.mr.requester.color
+                self.mr.cancel()
+            else:
+                self.mr.cancel()
         
 
-    def draw(self,win):
+    def draw(self, win, draw_temperature=False):
+        global temperatures_map_alpha
+        global temperatures
+        
         pygame.draw.rect(win,self.color,(self.x,self.y,side,side))
+        if draw_temperature:
+            unit_tempr = temperatures[self.Y][self.X]
+            tempr_color = {"R":0, "G":0, "B":0} #colors displayed in temperatures map
+            if unit_tempr > 0:
+                if unit_tempr > 255:
+                    tempr_color["R"] = 255
+                    tempr_color["G"] += unit_tempr - 255
+                    tempr_color["B"] += unit_tempr - 255
+                    if tempr_color["B"] > 255:
+                        tempr_color["G"] = 255
+                        tempr_color["B"] = 255
+                else:
+                    tempr_color["R"] = unit_tempr
+            else:
+                if unit_tempr < -255:
+                    tempr_color["B"] = 255
+                    tempr_color["G"] += -unit_tempr - 255
+                    tempr_color["R"] += -unit_tempr - 255
+                    if tempr_color["R"] > 255:
+                        tempr_color["G"] = 255
+                        tempr_color["R"] = 255
+                else:
+                    tempr_color["B"] = -unit_tempr
+
+            tempr_color["R"] = (self.particle.color[0] * (255 - temperatures_map_alpha) + tempr_color["R"] * temperatures_map_alpha) / 255
+            tempr_color["G"] = (self.particle.color[1] * (255 - temperatures_map_alpha) + tempr_color["G"] * temperatures_map_alpha) / 255
+            tempr_color["B"] = (self.particle.color[2] * (255 - temperatures_map_alpha) + tempr_color["B"] * temperatures_map_alpha) / 255
+            #it calculates the alpha value with a ponderated average
+                    
+            pygame.draw.rect(win,(tempr_color["R"], tempr_color["G"], tempr_color["B"]),(self.x,self.y,side,side))
 
 
 #==============================================PROGRAM===============================================
@@ -267,16 +316,19 @@ for y in range(0,len(world)):
         row.append(Unit(world[x][y], x*side, y*side, terrain, x,y))
     terrain.append(row)
 
-for y in range(0,len(world)):
+for y in range(0,len(world)): #just to get all the neighbours of the units before execution
     for x in range(0,len(world[0])):
         terrain[x][y].calc_neighbours()
 print("done generating the terrain matrix")
 
-    
-def updateGFX():
+   
+def updateGFX(temperatures_map=False):
     for row in terrain:
         for unit in row:
-            unit.draw(win)
+            if temperatures_map:
+                unit.draw(win, True)
+            else:
+                unit.draw(win)
     pygame.display.update()
 
 def update():
@@ -285,23 +337,68 @@ def update():
             unit.update()
             
 def late_update():
+    global temperatures
+    
     for row in terrain:
         for unit in row:
             unit.late_update()
+
+    if random.randint(0,10) == 0:
+        temperatures = [[tempr + random.randint(-1,3) for tempr in row] for row in temperatures]
 
 
 print("starting the mainloop...\n\n\nTABLE OF ELEMENTS:\n")
 print_elem_list(elements)
 
 run = True
+simulation_update_time = 0
+controls_update_time = 0
 while run:
+    temperatures_map = False
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    keys = pygame.key.get_pressed()
 
-    update()
-    late_update()
-    updateGFX()
+    if keys[pygame.K_p]: #shows additional infos in the console
+        if optional_prints == True:
+            optional_prints = False
+        else:
+            optional_prints = True
+        time.sleep(0.8)
+
+    if keys[pygame.K_t]: #shows the temperatures map
+        temperatures_map = True
+        if controls_update_time == 0:
+            if keys[pygame.K_KP_PLUS]: #adjusts the alpha value of the temperatures map
+                temperatures_map_alpha += 1
+                if temperatures_map_alpha > 255:
+                    temperatures_map_alpha = 255
+            if keys[pygame.K_KP_MINUS]:
+                temperatures_map_alpha -= 1
+                if temperatures_map_alpha < 0:
+                    temperatures_map_alpha = 0
+
+    if keys[pygame.K_d]:
+        for row in terrain:
+            for unit in row:
+                    print(unit)
+        print()
+        time.sleep(0.8)
+
+    if simulation_update_time == 0:
+        update()
+        late_update()
+        updateGFX(temperatures_map)
+
+    simulation_update_time += 1
+    if simulation_update_time > simulation_slowness_multiplier:
+        simulation_update_time = 0
+    controls_update_time += 1
+    if controls_update_time > control_sensibility_slowness_multiplier:
+        controls_update_time = 0
+
 
 print("\n\nexited the mainloop")
 print("exiting code sequence...")
